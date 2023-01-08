fe68e62b25876c99a3b971bf8d0d7493e312a76b
Deividson Sabino <xdeividsabinox@live.com>
2023-01-07 23:31:05 -0300

    Update Target.cs

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
+    private Rigidbody targetTb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        targetTb = GetComponent<Rigidbody>();
+
+        targetTb.AddForce(Vector3.up * Random.Range(12, 16), ForceMode.Impulse);
+        targetTb.AddTorque(Random.Range(-10,10), Random.Range(-10,10), Random.Range(-10, 10), ForceMode.Impulse);
+
+        transform.position = new Vector3(Random.Range(-4,4), -6);
     }
 
     // Update is called once per frame
